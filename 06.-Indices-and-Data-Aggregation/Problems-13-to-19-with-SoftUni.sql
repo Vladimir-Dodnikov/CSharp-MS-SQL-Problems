28a5594fdd42593dc9dbcc0230c8d6d1a56165cf
unknown <vladi_dodnikov@mail.bg>
2020-06-10 22:09:38 +0300

    Add 17-19 problems.

--- 06.-Indices-and-Data-Aggregation/Problems-13-to-19-with-SoftUni.sql
+++ 06.-Indices-and-Data-Aggregation/Problems-13-to-19-with-SoftUni.sql
@@ -37,4 +37,30 @@ SELECT DepartmentID, Max(Salary) AS [MaxSalary]
 	FROM Employees
 	WHERE Salary < 30000 OR Salary > 70000
 	GROUP BY DepartmentID
-	
+
+--Problem - 17
+SELECT COUNT(Salary) AS [Count]
+	FROM Employees
+	WHERE ManagerID IS NULL
+	GROUP BY ManagerID
+
+--Problem - 18
+SELECT DepartmentID, Salary AS [ThirdHighestSalary]
+	FROM (
+			SELECT DepartmentID, Salary, DENSE_RANK() OVER (PARTITION BY DepartmentID ORDER BY Salary DESC) AS SalaryRank
+				FROM Employees
+				GROUP BY DepartmentID, Salary
+		 ) AS [SalaryRankQuery]
+	WHERE SalaryRank = 3
+
+--Problem - 19
+SELECT TOP(10) FirstName, LastName, DepartmentID 
+	FROM Employees AS e1
+	WHERE e1.Salary > (
+						SELECT AVG(Salary)
+							FROM Employees AS e2
+							WHERE e1.DepartmentID = e2.DepartmentID	
+							GROUP BY DepartmentID	
+					  )
+	ORDER BY DepartmentID ASC
+
